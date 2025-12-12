0652e3cf0a9c3dd21f67bdab2be04ddebc9ef1d7
Guilherme Rubrae <143297561+guilhermerubrae@users.noreply.github.com>
2025-12-12 00:14:54 -0300

    Update Component.java

--- src/main/java/br/com/vidaplus/sghss/uninter/util/Component.java
+++ src/main/java/br/com/vidaplus/sghss/uninter/util/Component.java
@@ -1,5 +1,12 @@
 package br.com.vidaplus.sghss.uninter.util;
 
+/*
+ * anotação customizada para marcar componentes no projeto
+ * author: guilherme abreu
+ * comentários em minúsculas
+ */
 public @interface Component {
-
+    // essa anotação pode ser usada para identificar classes como componentes
+    // em um contexto de injeção de dependência, similar ao @Component do spring
 }
+
